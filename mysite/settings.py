bb8dd4535165d945041cbc392d00633e0bbe7eb1
Adityapatil21159780 <202516.sc@gmail.com>
2025-02-26 13:49:00 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,6 +113,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'dbms@123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
