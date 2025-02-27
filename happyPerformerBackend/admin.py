d89dbac25ff7234eae2f3996b160aa71557908ad
Adityapatil21159780 <202516.sc@gmail.com>
2025-02-27 22:33:48 +0530

    Update admin.py

--- happyPerformerBackend/admin.py
+++ happyPerformerBackend/admin.py
@@ -1,14 +1,12 @@
-<<<<<<< HEAD
+
 from django.contrib import admin
 
 # Register your models here.
 from .models import Employee  # Import Employee model
 
-=======
 from django.contrib import admin
 
 # Register your models here.
 from .models import Employee  # Import Employee model
 
->>>>>>> 174a167 (backend1)
 admin.site.register(Employee)  # Register Employee in admin
